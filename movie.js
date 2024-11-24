24957aacd17e3104c723143aecd9460c7c7d16e2
Mark <courtademark@gmail.com>
2024-11-23 18:23:23 -0700

    Almost done with js.

--- movie.js
+++ movie.js
@@ -1,45 +1,43 @@
 // https://www.omdbapi.com/?apikey=2022fb1c&s=fast
 
-let movies;
+const moviesWrapper = document.querySelector(".movies");
 
 async function renderMovies(filter) {
-    const moviesWrapper = document.querySelector(".movies");
+    const title = document.querySelector(".movie__search--bar").value;
+    const movies = await fetch(
+      `https://www.omdbapi.com/?apikey=2022fb1c&s=${title || ''}`
+    );
+    const movieData = await movies.json();
+    const result = movieData.Search;
 
-    moviesWrapper.classList += ' movies__loading';
+    console.log(result);
 
-    if (!movies) {
-        movies = await getMovies();
-    }
+  if (filter === "old_to_new") {
+    result.sort((a, b) => b.Year - a.Year);
+  } 
+  if (filter === "new_to_old") {
+    result.sort((a, b) => a.Yeat - b.Year);
+  }
 
-    moviesWrapper.classList.remove('movies__loading')
-
-    if (filter === "old_to_new") {
-        movies.sort((a, b) => (b.year - a.year));
-    }
-    else if (filter === "new_to_old") {
-        movies.sort((a, b) => (a.yeat - b.year));
-    }
-
-    const moviesHtml = movies.map((movie) => {
-       return `<div class="movie click">
+  moviesWrapper.innerHTML = result.map((movie) => {
+      return `<div class="movie click">
               <figure class="movie__img--wrapper">
                 <img src="${movie.Poster}" alt="" class="movie__img" />
               </figure>
               <div class="movie__details">
-                <div class="movie__title">Movie:${movie.Title}</div>
-                <div class="movie__year">year:${movie.Year}</div>
+                <div class="movie__title">Movie: ${movie.Title}</div>
+                <div class="movie__year">Year: ${movie.Year}</div>
               </div>
             </div>`;
     })
     .join("");
 
-    moviesWrapper.innerHTML = moviesHtml;
 }
 
-function filterMovies(event) {
+function filterMovies(event){
     renderMovies(event.target.value)
 }
 
 setTimeout(() => {
-    renderMovies();
+  renderMovies();
 });
