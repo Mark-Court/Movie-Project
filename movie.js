be69031d031ac6808b41c2f43e2f3e8b93c64cc8
Mark <courtademark@gmail.com>
2024-11-25 08:37:52 -0700

    Fixed problems

--- movie.js
+++ movie.js
@@ -1,29 +1,39 @@
 // https://www.omdbapi.com/?apikey=2022fb1c&s=fast
 
 const moviesWrapper = document.querySelector(".movies");
+const form = document.querySelector(".search__wrapper");
+const movieName = document.querySelector('.movie__search--bar');
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const movieNameValue = movieName.value;
+
+  sessionStorage.setItem("movie_title", movieNameValue);
+});
 
 async function renderMovies(filter) {
   const movieTitle = sessionStorage.getItem("movie_title");
-  const title = (document.querySelector(".movie__search--bar").value ||
-    movieTitle);
+  const title = movieTitle;
+
   const movies = await fetch(
     `https://www.omdbapi.com/?apikey=2022fb1c&s=${title || ""}`
   );
   const movieData = await movies.json();
   const result = movieData.Search;
 
-console.log(title)
 
-  if (filter === "old_to_new") {
+  
+  if (filter === "new_to_old") {
     result.sort((a, b) => b.Year - a.Year);
   }
-  if (filter === "new_to_old") {
-    result.sort((a, b) => a.Yeat - b.Year);
+  if (filter === "old_to_new") {
+    result.sort((a, b) => a.Year - b.Year);
   }
 
   moviesWrapper.innerHTML = result
     .map((movie) => {
-      return `<div class="movie click">
+      return `<div class="movie no__click">
               <figure class="movie__img--wrapper">
                 <img src="${movie.Poster}" alt="" class="movie__img" />
               </figure>
